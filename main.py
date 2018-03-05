355a7d728a303ecca3c47625926523bed4adc787
alien981 <dsparakis@stuy.edu>
2018-03-05 00:47:16 -0500

    did stuff

--- main.py
+++ main.py
@@ -10,4 +10,23 @@ transform = new_matrix()
 ident(transform)
 
 
-parse_file( 'script', edges, transform, screen, color )
+file = open('script', "w")
+s = ''
+q = -150
+while (q<=150):
+	s += 'line\n250 250 0 ' + str(q+250) + ' 400 -150\n'
+	s += 'line\n250 250 0 ' + str(250+q) + ' 100 -150\n'
+	s += 'line\n250 250 0 400 ' + str(q+250) + ' -150\n'
+	s += 'line\n250 250 0 100 ' + str(q+250) + ' -150\n'
+	s += 'line\n250 250 0 ' + str(q+250) + ' 400 150\n'
+	s += 'line\n250 250 0 ' + str(250+q) + ' 100 150\n'
+	s += 'line\n250 250 0 400 ' + str(q+250) + ' 150\n'
+	s += 'line\n250 250 0 100 ' + str(q+250) + ' 150\n'
+	q += 10
+s += 'rotate\nx 45\nrotate\ny 45\nmove\n-50 50 0\napply\n'
+s+= 'save\nimage'
+file.write(s)
+file.close()
+
+parse_file('script', edges, transform, screen, color )
+
--- parser.py
+++ parser.py
@@ -64,7 +64,7 @@ def parse_file( fname, points, transform, screen, color ):
 			w += 2
 		else:
 			w += 1
-
+		file.close()
 
 
 
